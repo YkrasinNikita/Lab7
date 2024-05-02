cf0e3290ab7996d11e0c0c447e077c1a1978901e
YkrasinNikita <148620189+YkrasinNikita@users.noreply.github.com>
2024-05-02 12:49:58 +0300

    Update ClassDate8Variant.cs
    проб

--- ClassLibrary3/ClassDate8Variant.cs
+++ ClassLibrary3/ClassDate8Variant.cs
@@ -7,15 +7,13 @@ namespace ClassLibaryTPO8VAR
     public class Smena
     {
         private string _nazvanie;
-
         public Smena(string nazvanie, DateTime? date = null)
         {
             _nazvanie = nazvanie;
             if (date == null)
             {
                 date = DateTime.Now;
-            }
-            
+            } 
         }
     }
 }
